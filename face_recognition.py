c4941487b522baf27275dd7e9820e695c2fe7b0a
Aryan . Curiel <aryan.curiel@MUSALA.COM>
2020-01-03 17:45:05 +0200

    Initial commit

---
+++ face_recognition.py
@@ -0,0 +1,42 @@
+import cv2
+import sys
+
+# cascPath = sys.argv[1]
+face_cascade = cv2.CascadeClassifier('cascades\\haarcascade_frontalface_default.xml')
+smile_cascade = cv2.CascadeClassifier('cascades\\haarcascade_smile.xml')
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    _, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.5,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        roi_gray = gray[y:y + h, x:x + w] 
+        roi_color = frame[y:y + h, x:x + w] 
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.8, 20) 
+  
+        for (sx, sy, sw, sh) in smiles: 
+            cv2.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
